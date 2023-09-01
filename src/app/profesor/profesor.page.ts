0d843e58cf3c13d01ed1e2f387131567c4709b4b
Camilo Pulgar Vega <camilopulgarv@gmail.com>
2023-09-01 16:13:32 -0400

    Primer Backend

--- src/app/profesor/profesor.page.ts
+++ src/app/profesor/profesor.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { UserModel } from '../models/UserModel';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profesor',
@@ -12,7 +14,11 @@ import { IonicModule } from '@ionic/angular';
 })
 export class ProfesorPage implements OnInit {
 
-  constructor() { }
+  adminInfoReceived: UserModel | undefined;
+
+  constructor(private r: Router) {
+    this.adminInfoReceived = this.r.getCurrentNavigation()?.extras.state?.['user'];
+   }
 
   ngOnInit() {
   }
